365cfbc7521d33ac157937f74ad5ba8048e2aae8
bebopkenny <kennygarcia15@csu.fullerton.edu>
2025-06-11 01:23:53 -0700

    Users have api keys generated by sha256

--- handler_user.go
+++ handler_user.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/bebopkenny/GoCrawler/internal/auth"
 	"github.com/bebopkenny/GoCrawler/internal/database"
 	"github.com/google/uuid"
 )
@@ -32,6 +33,22 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Couldn't create user: %v", err))
 		return 
+	}
+
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) { // Respond if the server is alive and running
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
+		return
 	}
 
 	respondWithJSON(w, 200, databaseUserToUser(user))
