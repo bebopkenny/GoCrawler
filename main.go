9428d55480fe9fcb927d33c52762ed4472a7edb3
bebopkenny <kennygarcia15@csu.fullerton.edu>
2025-06-10 23:26:04 -0700

    Users are able to enter their name. Now setting up the api key

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/go-chi/cors"
+	"github.com/bebopkenny/GoCrawler/internal/database"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load()
@@ -16,6 +24,21 @@ func main() {
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the enviroment")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the enviroment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err!= nil {
+		log.Fatal("Can't connect to database", err)
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
+	
 	}
 
 	router := chi.NewRouter()
@@ -33,6 +56,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness) // Connecting the handlerReady to the /ready path
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 	
@@ -42,7 +66,7 @@ func main() {
 	}
 
 	log.Printf("Server starting on port %v", portString)
-	err := srv.ListenAndServe() // Our server stops here and starts listening to http requests
+	err = srv.ListenAndServe() // Our server stops here and starts listening to http requests
 	if err != nil {
 		log.Fatal(err) // Display the error
 	}
